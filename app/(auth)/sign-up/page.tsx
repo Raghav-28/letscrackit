1b78ec3bd6d1d7b92610e0454c40f54c6f897414
Raghav-28 <kraghavgupta@gmail.com>
2025-04-08 17:02:25 +0530

    Created Basic Ui

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SignUp = () => {
+    return <AuthForm type="sign-up"/>
+}
+
+export default SignUp
---
+++ components/AuthForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AuthForm = () => {
+  return (
+    <div>
+      Hello bro
+    </div>
+  )
+}
+
+export default AuthForm
